8bb9b1d30c438642a7c03ca29d22db87b1ae2a51
Okekeifeanyi <okekeifeanyi20@yahoo.com>
2025-01-31 23:21:50 +0100

    restored

--- PensionNLPC/obj/Debug/net8.0/PensionNLPC.AssemblyInfo.cs
+++ PensionNLPC/obj/Debug/net8.0/PensionNLPC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PensionNLPC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df5703453d9816ed8fba7fabaf23dcb1a9b3f8f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8a03f22531197806bab12e19e6fac602a99233")]
 [assembly: System.Reflection.AssemblyProductAttribute("PensionNLPC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PensionNLPC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
